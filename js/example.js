65ee7fb48b91ba37565534ee2275bc53bc88e3f9
Nicolas Cajelli <nicos.web@gmail.com>
2015-08-02 23:15:55 -0300

    Removed hardcoded options, added localstorage

--- js/example.js
+++ js/example.js
@@ -1,24 +1,58 @@
-var pplArrayT = [];
-for (var k in optionsArray) {
-    pplArrayT.push(optionsArray[k]);
-}
 
 var resize = function() {
+
    var size;
 
    if (window.innerWidth > window.innerHeight) {
-       size = window.innerHeight;
-    } else {
-        size = window.innerWidth;
-    }
+	   size = window.innerHeight;
+	} else {
+		size = window.innerWidth;
+	}
    $(".canvas").width(size);
    $(".canvas").height(size);
 }
 
+var startWheel = function(optionsArray) {
+	var pplArrayT = [];
+	for (var k in optionsArray) {
+		pplArrayT.push(optionsArray[k]);
+	}
+	$(".canvas").removeClass("hide");
+	$('.canvas').spinwheel({
+		pplArray : pplArrayT
+	});
+
+}
+var getOptionsArray = function() {
+	if ($.localStorage.get("options")) {
+		return $.localStorage.get("options");
+	} else {
+		return [];
+	}
+}
+
+var askForOptions = function(callback) {
+	$("#ask-options").removeClass("hide");
+	var optionsArray = [];
+	$("#add-option").click(function() {
+		optionsArray.push($("#option").val());
+		$("#option").val("");
+
+	});
+	$("#start-wheel").click(function() {
+		$.localStorage.set("options", optionsArray);
+		$("#ask-options").addClass("hide");
+		callback(optionsArray);
+	});
+}
 $(document).ready(function(){
-    resize();
-   $(window).resize(resize);
-   $('.canvas').spinwheel({
-       pplArray : pplArrayT
-   });
+	resize();
+	$(window).resize(resize);
+
+	var optionsArray = getOptionsArray();
+	if (optionsArray.length) {
+		startWheel(optionsArray);
+	} else {
+		askForOptions(startWheel);
+	}
 });
