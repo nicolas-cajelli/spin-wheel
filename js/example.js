fe042f21a8932739ecbbc2c87d34465b504aeb71
Nicolas Cajelli <nicos.web@gmail.com>
2015-08-04 21:31:59 -0300

    Added current options list visible all time. Allow user to remove single option from list

--- js/example.js
+++ js/example.js
@@ -32,9 +32,10 @@ var getOptionsArray = function() {
 }
 
 var askForOptions = function(callback) {
+	$(".canvas").addClass("hide");
 	$("#ask-options").removeClass("hide");
-	var optionsArray = [];
 	$("#add-option").click(function() {
+		addOption($("#option").val());
 		optionsArray.push($("#option").val());
 		$("#option").val("");
 
@@ -45,12 +46,46 @@ var askForOptions = function(callback) {
 		callback(optionsArray);
 	});
 }
+var removeOption = function() {
+	var parent = $(this).parent();
+	var option = parent.find(".value").text();
+	var newOptions = [];
+	console.log(optionsArray);
+	console.log(option);
+	for (var idx in optionsArray) {
+		if (optionsArray[idx] != option) {
+			newOptions.push(optionsArray[idx]);
+		}
+	}
+	optionsArray = newOptions;
+	console.log(optionsArray);
+	$.localStorage.set("options", optionsArray);
+	parent.remove();
+	if (newOptions.length) {
+		startWheel(newOptions);
+	} else {
+		askForOptions(startWheel);
+	}
+}
+var addOption = function(option) {
+	var container = $("#options-template").clone();
+	container.find(".value").text(option);
+	container.attr("id", "");
+
+	container.find("a").click(removeOption);
+	$("#options").append(container);
+	container.removeClass("hide");
+}
+var optionsArray;
 $(document).ready(function(){
 	resize();
 	$(window).resize(resize);
 
-	var optionsArray = getOptionsArray();
+	optionsArray = getOptionsArray();
 	if (optionsArray.length) {
+		for (var idx in optionsArray) {
+			addOption(optionsArray[idx]);
+		}
 		startWheel(optionsArray);
 	} else {
 		askForOptions(startWheel);
@@ -58,7 +93,9 @@ $(document).ready(function(){
 
 	$("#clear-options").click(function() {
 		$.localStorage.remove("options");
-		$(".canvas").addClass("hide");
+		$(".option").each(function() {
+				$(this).remove();
+		});
 		askForOptions(startWheel);
 	});
 });
