659cb64c1c09ba69f28679bbf8490b8e5043c9c4
Tato Panelati <tatupane@gmail.com>
2015-08-08 18:41:11 -0300

    Added mailing library and set form sending to nicolas.cajelli@olx.com

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,8 +1,10 @@
 <?php
+
+require 'PHPMailer/PHPMailerAutoload.php';
+
 // Check for empty fields
 if(empty($_POST['name'])  		||
    empty($_POST['email']) 		||
-   empty($_POST['phone']) 		||
    empty($_POST['message'])	||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
@@ -12,15 +14,45 @@ if(empty($_POST['name'])  		||
 	
 $name = $_POST['name'];
 $email_address = $_POST['email'];
-$phone = $_POST['phone'];
 $message = $_POST['message'];
 	
 // Create the email and send the message
-$to = 'yourname@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";	
-mail($to,$email_subject,$email_body,$headers);
-return true;			
+
+  $mail = new PHPMailer;
+  //$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+  $mail->isSMTP();                                      // Set mailer to use SMTP
+  $mail->Host = 'smtp.mailgun.org';  // Specify main and backup SMTP servers
+  $mail->SMTPAuth = true;                               // Enable SMTP authentication
+  $mail->Username = 'postmaster@sandbox6bff932344484bcf851ecfe83848386d.mailgun.org';                 // SMTP username
+  $mail->Password = '26fcf27c0bb2344841d03519f5158824';                           // SMTP password
+  $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+  $mail->Port = 587;                                    // TCP port to connect to
+
+  $mail->From = $email_address;
+  $mail->FromName = $name;
+  $mail->addAddress('nicolas.cajelli@olx.com', 'Nico');     // Add a recipient
+  //$mail->addAddress('tatopane@gmail.com', 'Nico');     // Add a recipient
+  //$mail->addReplyTo('info@example.com', 'Information');
+  //$mail->addCC('cc@example.com');
+  //$mail->addBCC('bcc@example.com');
+
+  //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+  //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+  $mail->isHTML(true);                                  // Set email format to HTML
+
+  $mail->Subject = 'Nuevo contacto en QueSale';
+  $mail->Body    = $message;
+  $mail->AltBody = $message;
+
+  if(!$mail->send()) {
+      //echo 'Message could not be sent.';
+      //echo 'Mailer Error: ' . $mail->ErrorInfo;
+    return false;
+  } else {
+      //echo 'Message has been sent';
+      return true;
+  }
+
+			
 ?>
